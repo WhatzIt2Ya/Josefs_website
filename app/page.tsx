ecd0daa0f491513e36e4cafb1e2118c069e51f0c
WhatzIt2Ya <josefmpr@gmail.com>
2026-05-11 11:11:20 +1000

    fixed duplicated project description

--- app/page.tsx
+++ app/page.tsx
@@ -376,7 +376,6 @@ export default function Portfolio() {
             <div className="w-full md:w-1/2">
               <h3 className="text-2xl font-bold">{project.title}</h3>
               <h3 className="text-white font-semibold">{project.role}</h3>
-              <p className="text-gray-300 mt-2">{project.description}</p>
               <ul className="mt-2 text-gray-300 list-disc pl-5 space-y-1">
               {project.description.split("\n").map((line, i) => (
                 <li key={i}>{line}</li>
